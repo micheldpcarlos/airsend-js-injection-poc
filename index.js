2bc7266acc6404e8dd46bce8e8702eb16a674e65
Michel Carlos <micheldpcarlos@gmail.com>
2022-02-23 16:33:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log(this);
-document.write("<h1>Airsend Hacked</h1>");
-document.write("<h3>Proof of concept in progress...</h3>");
+alert("Code Injected");
+this.document.write("<h1>Airsend Hacked</h1>");
+this.document.write("<h3>Proof of concept in progress...</h3>");
